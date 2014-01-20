c27da6eb069294102c255d2605d79083629239ad
zoie0312 <greenmoonshisu@gmail.com>
2014-01-20 02:39:18 +0000

    Works locally for the first time

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var path = require("path");
+
+var app = express()
+            .use(express.static(__dirname,
+                                path.join(__dirname, "bower_components")))
+            .use(express.bodyParser())
+            .use(express.logger('dev'));
+
+app.get("/", function(req, res){
+    res.send("Hello, World");
+});
+
+var port = process.env.PORT || 8000;
+app.listen(port);
+console.log("Started listen on port " + port);
